813c6e8cf0c61a9cbe72f6abeaa250dccb14155c
Mark Brown <broonie@opensource.wolfsonmicro.com>
2011-09-28 18:23:53 +0100

    ASoC: Ensure all DAPM widgets have a power check callback
    Makes the code simpler.
    
    Signed-off-by: Mark Brown <broonie@opensource.wolfsonmicro.com>

--- sound/soc/soc-dapm.c
+++ sound/soc/soc-dapm.c
@@ -859,6 +859,11 @@ static int dapm_supply_check_power(struct snd_soc_dapm_widget *w)
 	return power;
 }
 
+static int dapm_always_on_check_power(struct snd_soc_dapm_widget *w)
+{
+	return 1;
+}
+
 static int dapm_seq_compare(struct snd_soc_dapm_widget *a,
 			    struct snd_soc_dapm_widget *b,
 			    bool power_up)
@@ -1231,9 +1236,6 @@ static void dapm_power_one_widget(struct snd_soc_dapm_widget *w,
 		break;
 
 	default:
-		if (!w->power_check)
-			break;
-
 		if (!w->force)
 			power = w->power_check(w);
 		else
@@ -2079,6 +2081,9 @@ int snd_soc_dapm_new_widgets(struct snd_soc_dapm_context *dapm)
 		case snd_soc_dapm_post:
 			break;
 		}
+
+		if (!w->power_check)
+			w->power_check = dapm_always_on_check_power;
 
 		/* Read the initial power state from the device */
 		if (w->reg >= 0) {
