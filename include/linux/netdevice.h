d95ecddfe4f42d749b74149f34ab5c200a6f8a97
David S. Miller <davem@davemloft.net>
2011-12-13 16:44:22 -0500

    net: Remove unused neighbour layer ops.
    It's simpler to just keep these things out until there is a real user
    of them, so we can see what the needs actually are, rather than keep
    these things around as useless overhead.
    
    Signed-off-by: David S. Miller <davem@davemloft.net>

--- include/linux/netdevice.h
+++ include/linux/netdevice.h
@@ -990,7 +990,6 @@ struct net_device_ops {
 	int			(*ndo_set_features)(struct net_device *dev,
 						    netdev_features_t features);
 	int			(*ndo_neigh_construct)(struct neighbour *n);
-	int			(*ndo_neigh_destroy)(struct neighbour *n);
 };
 
 /*
