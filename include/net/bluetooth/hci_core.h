ff05b27d2fb616198efd73d59755ff358ef8fb21
Andrei Emeltchenko <andrei.emeltchenko@intel.com>
2012-03-09 11:46:49 +0200

    Bluetooth: Correct type for hdev lmp_subver
    Keep lmp_subver in host byte order. We have following conversion
    in hci_cc_read_local_version:
    hdev->lmp_subver = __le16_to_cpu(rp->lmp_subver);
    
    Signed-off-by: Andrei Emeltchenko <andrei.emeltchenko@intel.com>
    Signed-off-by: Gustavo F. Padovan <padovan@profusion.mobi>

--- include/net/bluetooth/hci_core.h
+++ include/net/bluetooth/hci_core.h
@@ -175,7 +175,7 @@ struct hci_dev {
 	__u16		hci_rev;
 	__u8		lmp_ver;
 	__u16		manufacturer;
-	__le16		lmp_subver;
+	__u16		lmp_subver;
 	__u16		voice_setting;
 	__u8		io_capability;
 	__s8		inq_tx_power;
