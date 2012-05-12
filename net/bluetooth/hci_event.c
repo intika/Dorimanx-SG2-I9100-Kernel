0aa0f4937bceb1c21fa4aa12a53f220be49db82d
Johan Hedberg <johan.hedberg@intel.com>
2012-05-12 16:11:50 -0300

    Bluetooth: mgmt: Fix device_connected sending order
    The mgmt_ev_device_connected signal must be sent before any event
    indications happen for sockets associated with the connection. Otherwise
    e.g. device authorization for the sockets will fail with ENOTCONN as
    user space things that there is no baseband link.
    
    This patch fixes the issue by ensuring that the device_connected event
    if sent (if it hasn't been so already) as soon as the first ACL data
    packet arrives from the remote device.
    
    Signed-off-by: Johan Hedberg <johan.hedberg@intel.com>
    Acked-by: Marcel Holtmann <marcel@holtmann.org>
    Signed-off-by: John W. Linville <linville@tuxdriver.com>

--- net/bluetooth/hci_event.c
+++ net/bluetooth/hci_event.c
@@ -2241,7 +2241,7 @@ static inline void hci_remote_features_evt(struct hci_dev *hdev, struct sk_buff 
 		goto unlock;
 	}
 
-	if (!ev->status) {
+	if (!ev->status && !test_bit(HCI_CONN_MGMT_CONNECTED, &conn->flags)) {
 		struct hci_cp_remote_name_req cp;
 		memset(&cp, 0, sizeof(cp));
 		bacpy(&cp.bdaddr, &conn->dst);
@@ -3074,7 +3074,7 @@ static inline void hci_remote_ext_features_evt(struct hci_dev *hdev, struct sk_b
 	if (conn->state != BT_CONFIG)
 		goto unlock;
 
-	if (!ev->status) {
+	if (!ev->status && !test_bit(HCI_CONN_MGMT_CONNECTED, &conn->flags)) {
 		struct hci_cp_remote_name_req cp;
 		memset(&cp, 0, sizeof(cp));
 		bacpy(&cp.bdaddr, &conn->dst);
