16b39a7965da6ca123be34bfb82786c61479770d
Jiri Pirko <jpirko@redhat.com>
2012-06-26 06:52:46 +0000

    team: do not allow to map disabled ports
    Signed-off-by: Jiri Pirko <jpirko@redhat.com>
    Signed-off-by: David S. Miller <davem@davemloft.net>

--- include/linux/if_team.h
+++ include/linux/if_team.h
@@ -49,6 +49,8 @@ struct team_port {
 	struct rcu_head rcu;
 };
 
+extern bool team_port_enabled(struct team_port *port);
+
 struct team_mode_ops {
 	int (*init)(struct team *team);
 	void (*exit)(struct team *team);
