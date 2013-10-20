dbe610d98a09914748362feaec3c00208d0227c5
Dorimanx <yuri@bynet.co.il>
2013-10-20 16:41:55 +0300

    Added NEOX CPU Gov. now need to update it for my tree! :)
    https://github.com/neobuddy89/NX-Kernel-exynos4/commit/bbe3d7aff0f58c0512d20e3c7fa09977b8b62565

--- include/linux/cpufreq.h
+++ include/linux/cpufreq.h
@@ -415,6 +415,9 @@ extern struct cpufreq_governor cpufreq_gov_nightmare;
 #elif defined(CONFIG_CPU_FREQ_DEFAULT_GOV_DARKNESS)
 extern struct cpufreq_governor cpufreq_gov_darkness;
 #define CPUFREQ_DEFAULT_GOVERNOR	(&cpufreq_gov_darkness)
+#elif defined(CONFIG_CPU_FREQ_DEFAULT_GOV_NEOX)
+extern struct cpufreq_governor cpufreq_gov_neox;
+#define CPUFREQ_DEFAULT_GOVERNOR  (&cpufreq_gov_neox)
 #endif
 
 
