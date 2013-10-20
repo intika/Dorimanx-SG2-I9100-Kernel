196c636d14a8c9e1095e6f88742f05e3518441e3
Dorimanx <yuri@bynet.co.il>
2013-10-20 15:29:15 +0300

    Sync drivers/base/power/main.c to fix suspend fails.

--- drivers/base/power/main.c
+++ drivers/base/power/main.c
@@ -1155,7 +1155,6 @@ static int __device_suspend(struct device *dev, pm_message_t state, bool async)
 	timer.data = (unsigned long)&data;
 	add_timer(&timer);
 
-
 	if (async_error)
 		goto Complete;
 
@@ -1408,13 +1407,7 @@ int dpm_prepare(pm_message_t state)
 		get_device(dev);
 		mutex_unlock(&dpm_list_mtx);
 
-		pm_runtime_get_noresume(dev);
-		if (pm_runtime_barrier(dev) && device_may_wakeup(dev))
-			pm_wakeup_event(dev, 0);
-
-		pm_runtime_put_sync(dev);
-		error = pm_wakeup_pending() ?
-				-EBUSY : device_prepare(dev, state);
+		error = device_prepare(dev, state);
 
 		mutex_lock(&dpm_list_mtx);
 		if (error) {
