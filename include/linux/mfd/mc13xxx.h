0091d1b9c64866c09ff67a8c8dcd31ba0f7dc376
Mark Brown <broonie@linaro.org>
2013-11-15 10:20:00 +0000

    Merge remote-tracking branch 'asoc/fix/arizona' into asoc-linus
    Conflicts:
    	Documentation/sound/alsa/soc/DPCM.txt
    	Documentation/sound/alsa/soc/dapm.txt
    	drivers/mfd/mc13xxx-core.c
    	drivers/mfd/mc13xxx-spi.c
    	include/linux/mfd/mc13xxx.h
    	include/linux/platform_data/davinci_asp.h

--- include/linux/mfd/mc13xxx.h
+++ include/linux/mfd/mc13xxx.h
@@ -37,6 +37,20 @@ int mc13xxx_irq_ack(struct mc13xxx *mc13xxx, int irq);
 
 int mc13xxx_get_flags(struct mc13xxx *mc13xxx);
 
+<<<<<<< HEAD
+=======
+int mc13xxx_adc_do_conversion(struct mc13xxx *mc13xxx,
+		unsigned int mode, unsigned int channel,
+		u8 ato, bool atox, unsigned int *sample);
+
+#define MC13783_AUDIO_RX0	36
+#define MC13783_AUDIO_RX1	37
+#define MC13783_AUDIO_TX	38
+#define MC13783_SSI_NETWORK	39
+#define MC13783_AUDIO_CODEC	40
+#define MC13783_AUDIO_DAC	41
+
+>>>>>>> 092d405... Merge remote-tracking branch 'asoc/fix/arizona' into asoc-linus
 #define MC13XXX_IRQ_ADCDONE	0
 #define MC13XXX_IRQ_ADCBISDONE	1
 #define MC13XXX_IRQ_TS		2
