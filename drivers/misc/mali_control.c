43f9dd4faa04f986add0682ceeaa24cba9a1a16a
Dorimanx <yuri@bynet.co.il>
2013-11-28 10:55:00 +0200

    MALI CONTROL INT code style fix + limits correction.

--- drivers/misc/mali_control.c
+++ drivers/misc/mali_control.c
@@ -12,16 +12,16 @@
 #include <linux/miscdevice.h>
 #include <linux/kallsyms.h>
 
-#define GPU_MAX_CLOCK 700
-#define GPU_MIN_CLOCK 10
+#define GPU_MAX_CLOCK 520
+#define GPU_MIN_CLOCK 100
 
-#define MIN_VOLTAGE_GPU  600000
+#define MIN_VOLTAGE_GPU  800000
 #define MAX_VOLTAGE_GPU 1400000
 
 typedef struct mali_dvfs_tableTag{
-    unsigned int clock;
-    unsigned int freq;
-    unsigned int vol;
+	unsigned int clock;
+	unsigned int freq;
+	unsigned int vol;
 }mali_dvfs_table;
 
 typedef struct mali_dvfs_thresholdTag{
@@ -34,111 +34,90 @@ typedef struct mali_runtime_resumeTag{
 	int vol;
 } mali_runtime_resume_table;
 
-typedef struct mali_dvfs_staycount{
-	unsigned int staycount;
-}mali_dvfs_staycount_table;
-
 mali_runtime_resume_table *gm_mali_runtime_resume;
 mali_dvfs_table *gm_mali_dvfs;
 mali_dvfs_table *gm_mali_dvfs_all;
 mali_dvfs_threshold_table *gm_mali_dvfs_threshold;
-mali_dvfs_staycount_table *gm_mali_dvfs_staycount;
 int *gm_mali_dvfs_control;
 
 
-static ssize_t gpu_clock_show(struct device *dev, struct device_attribute *attr, char *buf) {
-	return sprintf(buf, "Step0: %d\nStep1: %d\nStep2: %d\nStep3: %d\nStep4: %d\n",
-		gm_mali_dvfs[0].clock, gm_mali_dvfs[1].clock, gm_mali_dvfs[2].clock, gm_mali_dvfs[3].clock, gm_mali_dvfs[4].clock
-		);
+static ssize_t gpu_clock_show(struct device *dev,
+			struct device_attribute *attr, char *buf)
+{
+	return sprintf(buf,
+			"Step0: %d\nStep1: %d\nStep2: %d\nStep3: %d\nStep4: %d\n",
+			gm_mali_dvfs[0].clock, gm_mali_dvfs[1].clock,
+			gm_mali_dvfs[2].clock, gm_mali_dvfs[3].clock,
+			gm_mali_dvfs[4].clock);
 }
 
-
-static ssize_t gpu_clock_store(struct device *dev, struct device_attribute *attr, const char *buf,
-									size_t count) {
+static ssize_t gpu_clock_store(struct device *dev,
+			struct device_attribute *attr,
+			const char *buf, size_t count)
+{
 	unsigned int ret = -EINVAL;
 	int i = 0;
 	unsigned int g[8];
 
-	if ( (ret=sscanf(buf, "%d %d %d %d %d", &g[0], &g[1], &g[2], &g[3], &g[4]))!=5 )
-			return -EINVAL;
+	if ((ret=sscanf(buf, "%d %d %d %d %d", &g[0], &g[1],
+			&g[2], &g[3], &g[4]))!=5)
+		return -EINVAL;
 	/* safety floor and ceiling - netarchy */
-	for( i = 0; i < 5; i++ ) {
+	for (i = 0; i < 5; i++) {
 		if (g[i] < GPU_MIN_CLOCK) {
 			g[i] = GPU_MIN_CLOCK;
-		}
-		else if (g[i] > GPU_MAX_CLOCK) {
+		} else if (g[i] > GPU_MAX_CLOCK) {
 			g[i] = GPU_MAX_CLOCK;
 		}
-		gm_mali_dvfs_all[i].clock=g[i];
+		gm_mali_dvfs_all[i].clock = g[i];
 	}
 	*gm_mali_dvfs_control = 999;
-	return count;	
+	return count;
 }
 
-static ssize_t gpu_staycount_show(struct device *dev, struct device_attribute *attr, char *buf) {
-	return sprintf(buf, "%d %d %d %d %d\n", 
-	gm_mali_dvfs_staycount[0].staycount,
-	gm_mali_dvfs_staycount[1].staycount,
-	gm_mali_dvfs_staycount[2].staycount,
-	gm_mali_dvfs_staycount[3].staycount,
-	gm_mali_dvfs_staycount[4].staycount
-	);
+static ssize_t gpu_voltage_show(struct device *dev,
+			struct device_attribute *attr, char *buf)
+{
+	return sprintf(buf,
+			"Step1: %d\nStep2: %d\nStep3: %d\nStep4: %d\nStep5: %d\n",
+			gm_mali_dvfs[0].vol, gm_mali_dvfs[1].vol,
+			gm_mali_dvfs[2].vol, gm_mali_dvfs[3].vol,
+			gm_mali_dvfs[4].vol);
 }
 
-static ssize_t gpu_staycount_store(struct device *dev, struct device_attribute *attr, const char *buf,
-									size_t count) {
-	unsigned int ret = -EINVAL;
-	int i1, i2, i3, i4, i5;
-
-    if ( (ret=sscanf(buf, "%d %d %d %d %d", &i1, &i2, &i3, &i4, &i5))!=5 )
-		return -EINVAL;
-	gm_mali_dvfs_staycount[0].staycount = i1;
-	gm_mali_dvfs_staycount[1].staycount = i2;
-	gm_mali_dvfs_staycount[2].staycount = i3;
-	gm_mali_dvfs_staycount[3].staycount = i4;
-	gm_mali_dvfs_staycount[4].staycount = i5;
-	return count;	
-}
-
-static ssize_t gpu_voltage_show(struct device *dev, struct device_attribute *attr, char *buf) {
-	return sprintf(buf, "Step1: %d\nStep2: %d\nStep3: %d\nStep4: %d\nStep5: %d\n",
-			gm_mali_dvfs[0].vol, gm_mali_dvfs[1].vol,gm_mali_dvfs[2].vol,gm_mali_dvfs[3].vol,gm_mali_dvfs[4].vol);
-}
-
-static ssize_t gpu_voltage_store(struct device *dev, struct device_attribute *attr, const char *buf,
-									size_t count) {
+static ssize_t gpu_voltage_store(struct device *dev,
+		struct device_attribute *attr, const char *buf, size_t count)
+{
 	unsigned int ret = -EINVAL;
 	int i = 0;
 	unsigned int gv[5];
 
-	ret = sscanf(buf, "%d %d %d %d %d", &gv[0], &gv[1], &gv[2], &gv[3], &gv[4]);
-	if(ret!=5) 
-	{
+	ret = sscanf(buf, "%d %d %d %d %d", &gv[0], &gv[1],
+			&gv[2], &gv[3], &gv[4]);
+	if (ret != 5) {
 		return -EINVAL;
 	}
-	
-    /* safety floor and ceiling - netarchy */
-    for( i = 0; i < 5; i++ ) {
-        if (gv[i] < MIN_VOLTAGE_GPU) {
-            gv[i] = MIN_VOLTAGE_GPU;
-        }
-        else if (gv[i] > MAX_VOLTAGE_GPU) {
-            gv[i] = MAX_VOLTAGE_GPU;
-    	}
+
+	/* safety floor and ceiling - netarchy */
+	for(i = 0; i < 5; i++) {
+		if (gv[i] < MIN_VOLTAGE_GPU) {
+			gv[i] = MIN_VOLTAGE_GPU;
+		} else if (gv[i] > MAX_VOLTAGE_GPU) {
+			gv[i] = MAX_VOLTAGE_GPU;
+		}
 		gm_mali_dvfs[i].vol=gv[i];
-    }
-//	gm_mali_runtime_resume.vol = gm_mali_dvfs[1].vol;
-	return count;	
+	}
+	return count;
 }
 
-static DEVICE_ATTR(voltage_control, S_IRUGO | S_IWUGO, gpu_voltage_show, gpu_voltage_store);
-static DEVICE_ATTR(clock_control, S_IRUGO | S_IWUGO, gpu_clock_show, gpu_clock_store);
-static DEVICE_ATTR(staycount_control, S_IRUGO | S_IWUGO, gpu_staycount_show, gpu_staycount_store);
+static DEVICE_ATTR(voltage_control, S_IRUGO |
+		S_IWUGO, gpu_voltage_show, gpu_voltage_store);
+static DEVICE_ATTR(clock_control, S_IRUGO |
+		S_IWUGO, gpu_clock_show, gpu_clock_store);
 
 static struct attribute *malicontrol_attributes[] = {
 	&dev_attr_voltage_control.attr,
 	&dev_attr_clock_control.attr,
-	&dev_attr_staycount_control.attr,
 	NULL
 };
 
@@ -153,26 +132,29 @@ static struct miscdevice malicontrol_device = {
 
 int register_mali_control(void)
 {
-    int ret;
-    pr_info("%s misc_register(%s)\n", __FUNCTION__, malicontrol_device.name);
+	int ret;
+	pr_info("%s misc_register(%s)\n",
+			__FUNCTION__, malicontrol_device.name);
 	gm_mali_dvfs = (mali_dvfs_table *)kallsyms_lookup_name("mali_dvfs");
-	gm_mali_dvfs_all = (mali_dvfs_table *)kallsyms_lookup_name("mali_dvfs_all");
-	gm_mali_dvfs_threshold = (mali_dvfs_threshold_table *)kallsyms_lookup_name("mali_dvfs_threshold");
-	gm_mali_dvfs_staycount = (mali_dvfs_staycount_table *)kallsyms_lookup_name("mali_dvfs_staycount");
-	gm_mali_dvfs_control = (int *)kallsyms_lookup_name("mali_dvfs_control");
-    ret = misc_register(&malicontrol_device);
-    if (ret) 
-	{
-	    pr_err("%s misc_register(%s) fail\n", __FUNCTION__, malicontrol_device.name);
-	    return 1;
+	gm_mali_dvfs_all = (mali_dvfs_table *)
+			kallsyms_lookup_name("mali_dvfs_all");
+	gm_mali_dvfs_threshold = (mali_dvfs_threshold_table *)
+			kallsyms_lookup_name("mali_dvfs_threshold");
+	gm_mali_dvfs_control = (int *)
+			kallsyms_lookup_name("mali_dvfs_control");
+	ret = misc_register(&malicontrol_device);
+	if (ret) {
+		pr_err("%s misc_register(%s) fail\n",
+				__FUNCTION__, malicontrol_device.name);
+		return 1;
 	}
-    if (sysfs_create_group(&malicontrol_device.this_device->kobj, &malicontrol_group) < 0) 
-	{
-	    pr_err("%s sysfs_create_group fail\n", __FUNCTION__);
-	    pr_err("Failed to create sysfs group for device (%s)!\n", malicontrol_device.name);
+	if (sysfs_create_group(&malicontrol_device.this_device->kobj,
+			&malicontrol_group) < 0) {
+		pr_err("%s sysfs_create_group fail\n", __FUNCTION__);
+		pr_err("Failed to create sysfs group for device (%s)!\n",
+				malicontrol_device.name);
 	}
-
-    return 0;
+	return 0;
 }
 
 static int __init malicontrol_init(void)
@@ -186,7 +168,8 @@ static int __init malicontrol_init(void)
 
 static void __exit malicontrol_exit(void)
 {
-	sysfs_remove_group(&malicontrol_device.this_device->kobj, &malicontrol_group);
+	sysfs_remove_group(&malicontrol_device.this_device->kobj,
+			&malicontrol_group);
 	misc_deregister(&malicontrol_device);
 }
 
