b8fef8b5395a63a9012e7e61f54502ea481725f8
Tony Lu <zlu@tilera.com>
2013-12-05 15:36:54 -0500

    ftrace: default to tilegx if ARCH=tile is specified
    This matches the existing behavior in arch/tile/Makefile for defconfig.
    
    Reported-by: fengguang.wu@intel.com
    Acked-by: Steven Rostedt <rostedt@goodmis.org>
    Signed-off-by: Tony Lu <zlu@tilera.com>
    Signed-off-by: Chris Metcalf <cmetcalf@tilera.com>

--- scripts/recordmcount.pl
+++ scripts/recordmcount.pl
@@ -364,6 +364,11 @@ if ($arch eq "x86_64") {
 } elsif ($arch eq "blackfin") {
     $mcount_regex = "^\\s*([0-9a-fA-F]+):.*\\s__mcount\$";
     $mcount_adjust = -4;
+} elsif ($arch eq "tilegx" || $arch eq "tile") {
+    # Default to the newer TILE-Gx architecture if only "tile" is given.
+    $mcount_regex = "^\\s*([0-9a-fA-F]+):.*\\s__mcount\$";
+    $type = ".quad";
+    $alignment = 8;
 } else {
     die "Arch $arch is not supported with CONFIG_FTRACE_MCOUNT_RECORD";
 }
