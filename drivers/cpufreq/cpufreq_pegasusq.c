55225db219ad7aaa0588627fa5658e5422e3472e
Dorimanx <yuri@bynet.co.il>
2013-12-08 22:43:14 +0200

    Reverted the Change the calculation of target frequency, seems UNSTABLE.
    this reverts this:
    https://github.com/dorimanx/Dorimanx-SG2-I9100-Kernel/commit/efc26e3a87c4c0e1a5194e49b7c011fff3bfba33
    
    without the NEOX hotplug restore and some cleanups that stay.

--- drivers/cpufreq/cpufreq_pegasusq.c
+++ drivers/cpufreq/cpufreq_pegasusq.c
@@ -482,14 +482,12 @@ static void dbs_check_cpu(struct cpu_dbs_info_s *this_dbs_info)
 	up_threshold = dbs_tuners_ins.up_threshold;
 
 	if (max_load_freq <
-	    (up_threshold - dbs_tuners_ins.down_differential) *
-			policy->cur) {
+	    (up_threshold - dbs_tuners_ins.down_differential) * policy->cur) {
 		unsigned int freq_next;
 		unsigned int down_thres;
 
 		freq_next = max_load_freq /
-			(up_threshold -
-				dbs_tuners_ins.down_differential);
+			(up_threshold - dbs_tuners_ins.down_differential);
 
 		/* No longer fully busy, reset rate_mult */
 		this_dbs_info->rate_mult = 1;
