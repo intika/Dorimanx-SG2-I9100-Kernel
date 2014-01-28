90452efd68d37586c72a4081b92b02084634a0f5
Linus Torvalds <torvalds@linux-foundation.org>
2014-01-28 11:02:23 -0800

    Merge branch 'for-linus' of git://git.kernel.org/pub/scm/linux/kernel/git/sage/ceph-client
    Pull ceph updates from Sage Weil:
     "This is a big batch.  From Ilya we have:
    
       - rbd support for more than ~250 mapped devices (now uses same scheme
         that SCSI does for device major/minor numbering)
       - crush updates for new mapping behaviors (will be needed for coming
         erasure coding support, among other things)
       - preliminary support for tiered storage pools
    
      There is also a big series fixing a pile cephfs bugs with clustered
      MDSs from Yan Zheng, ACL support for cephfs from Guangliang Zhao, ceph
      fscache improvements from Li Wang, improved behavior when we get
      ENOSPC from Josh Durgin, some readv/writev improvements from
      Majianpeng, and the usual mix of small cleanups"
    
    * 'for-linus' of git://git.kernel.org/pub/scm/linux/kernel/git/sage/ceph-client: (76 commits)
      ceph: cast PAGE_SIZE to size_t in ceph_sync_write()
      ceph: fix dout() compile warnings in ceph_filemap_fault()
      libceph: support CEPH_FEATURE_OSD_CACHEPOOL feature
      libceph: follow redirect replies from osds
      libceph: rename ceph_osd_request::r_{oloc,oid} to r_base_{oloc,oid}
      libceph: follow {read,write}_tier fields on osd request submission
      libceph: add ceph_pg_pool_by_id()
      libceph: CEPH_OSD_FLAG_* enum update
      libceph: replace ceph_calc_ceph_pg() with ceph_oloc_oid_to_pg()
      libceph: introduce and start using oid abstraction
      libceph: rename MAX_OBJ_NAME_SIZE to CEPH_MAX_OID_NAME_LEN
      libceph: move ceph_file_layout helpers to ceph_fs.h
      libceph: start using oloc abstraction
      libceph: dout() is missing a newline
      libceph: add ceph_kv{malloc,free}() and switch to them
      libceph: support CEPH_FEATURE_EXPORT_PEER
      ceph: add imported caps when handling cap export message
      ceph: add open export target session helper
      ceph: remove exported caps when handling cap import message
      ceph: handle session flush message
      ...

--- include/linux/crush/mapper.h
+++ include/linux/crush/mapper.h
@@ -14,7 +14,12 @@ extern int crush_find_rule(struct crush_map *map, int pool, int type, int size);
 extern int crush_do_rule(struct crush_map *map,
 			 int ruleno,
 			 int x, int *result, int result_max,
+<<<<<<< HEAD
 			 int forcefeed,    /* -1 for none */
 			 __u32 *weights);
+=======
+			 const __u32 *weights, int weight_max,
+			 int *scratch);
+>>>>>>> d891ea2... Merge branch 'for-linus' of git://git.kernel.org/pub/scm/linux/kernel/git/sage/ceph-client
 
 #endif
