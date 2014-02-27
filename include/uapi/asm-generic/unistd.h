641e252050ac0ac2b45d239a739ad60e7060af1e
Linus Torvalds <torvalds@linux-foundation.org>
2014-02-27 10:54:52 -0800

    Merge tag 'metag-fixes-v3.14' of git://git.kernel.org/pub/scm/linux/kernel/git/jhogan/metag
    Pull Metag arch and asm-generic fixes from James Hogan:
    
     - Add the new sched_setattr/sched_getattr syscalls to the asm-generic
       syscall list, which is used by arc, arm64, c6x, hexagon, metag,
      openrisc, score, tile, and unicore32.
    
     - An IRQ affinity bug fix for metag to prevent interrupts being
       vectored to offline CPUs when their affinity is changed via
       /proc/irq/ (thanks tglx).
    
    * tag 'metag-fixes-v3.14' of git://git.kernel.org/pub/scm/linux/kernel/git/jhogan/metag:
      irq-metag*: stop set_affinity vectoring to offline cpus
      asm-generic: add sched_setattr/sched_getattr syscalls

--- include/uapi/asm-generic/unistd.h
+++ include/uapi/asm-generic/unistd.h
@@ -690,9 +690,15 @@ __SC_COMP(__NR_process_vm_writev, sys_process_vm_writev, \
           compat_sys_process_vm_writev)
 #define __NR_kcmp 272
 __SYSCALL(__NR_kcmp, sys_kcmp)
+#define __NR_finit_module 273
+__SYSCALL(__NR_finit_module, sys_finit_module)
+#define __NR_sched_setattr 274
+__SYSCALL(__NR_sched_setattr, sys_sched_setattr)
+#define __NR_sched_getattr 275
+__SYSCALL(__NR_sched_getattr, sys_sched_getattr)
 
 #undef __NR_syscalls
-#define __NR_syscalls 273
+#define __NR_syscalls 276
 
 /*
  * All syscalls below here should go away really,
