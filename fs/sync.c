103c4d99e953eb3a1c19bb6cbad4277b12c8dfc4
Linus Torvalds <torvalds@linux-foundation.org>
2014-02-27 10:37:22 -0800

    Merge branch 'for_linus' of git://git.kernel.org/pub/scm/linux/kernel/git/jack/linux-fs
    Pull filesystem fixes from Jan Kara:
     "Notification, writeback, udf, quota fixes
    
      The notification patches are (with one exception) a fallout of my
      fsnotify rework which went into -rc1 (I've extented LTP to cover these
      cornercases to avoid similar breakage in future).
    
      The UDF patch is a nasty data corruption Al has recently reported,
      the revert of the writeback patch is due to possibility of violating
      sync(2) guarantees, and a quota bug can lead to corruption of quota
      files in ocfs2"
    
    * 'for_linus' of git://git.kernel.org/pub/scm/linux/kernel/git/jack/linux-fs:
      fsnotify: Allocate overflow events with proper type
      fanotify: Handle overflow in case of permission events
      fsnotify: Fix detection whether overflow event is queued
      Revert "writeback: do not sync data dirtied after sync start"
      quota: Fix race between dqput() and dquot_scan_active()
      udf: Fix data corruption on file type conversion
      inotify: Fix reporting of cookies for inotify events

--- fs/sync.c
+++ fs/sync.c
@@ -32,11 +32,10 @@ extern bool dyn_fsync_active;
  * wait == 1 case since in that case write_inode() functions do
  * sync_dirty_buffer() and thus effectively write one block at a time.
  */
-static int __sync_filesystem(struct super_block *sb, int wait,
-			     unsigned long start)
+static int __sync_filesystem(struct super_block *sb, int wait)
 {
 	if (wait)
-		sync_inodes_sb(sb, start);
+		sync_inodes_sb(sb);
 	else
 		writeback_inodes_sb(sb, WB_REASON_SYNC);
 
@@ -53,7 +52,6 @@ static int __sync_filesystem(struct super_block *sb, int wait,
 int sync_filesystem(struct super_block *sb)
 {
 	int ret;
-	unsigned long start = jiffies;
 
 	/*
 	 * We need to be protected against the filesystem going from
@@ -67,17 +65,17 @@ int sync_filesystem(struct super_block *sb)
 	if (sb->s_flags & MS_RDONLY)
 		return 0;
 
-	ret = __sync_filesystem(sb, 0, start);
+	ret = __sync_filesystem(sb, 0);
 	if (ret < 0)
 		return ret;
-	return __sync_filesystem(sb, 1, start);
+	return __sync_filesystem(sb, 1);
 }
 EXPORT_SYMBOL_GPL(sync_filesystem);
 
 static void sync_inodes_one_sb(struct super_block *sb, void *arg)
 {
 	if (!(sb->s_flags & MS_RDONLY))
-		sync_inodes_sb(sb, *((unsigned long *)arg));
+		sync_inodes_sb(sb);
 }
 
 static void sync_fs_one_sb(struct super_block *sb, void *arg)
@@ -114,10 +112,9 @@ static void fdatawait_one_bdev(struct block_device *bdev, void *arg)
 static void do_sync(void)
 {
 	int nowait = 0, wait = 1;
-	unsigned long start = jiffies;
 
 	wakeup_flusher_threads(0, WB_REASON_SYNC);
-	iterate_supers(sync_inodes_one_sb, &start);
+	iterate_supers(sync_inodes_one_sb, NULL);
 	iterate_supers(sync_fs_one_sb, &nowait);
 	iterate_supers(sync_fs_one_sb, &wait);
 	iterate_bdevs(fdatawrite_one_bdev, NULL);
