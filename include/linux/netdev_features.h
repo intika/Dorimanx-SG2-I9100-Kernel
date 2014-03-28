659e7294a76e04e4941b425e86db2155457968de
David S. Miller <davem@davemloft.net>
2014-03-28 17:17:16 -0400

    Merge branch 'vlan_offloads'
    Vlad Yasevich says:
    
    ====================
    Audit all drivers for correct vlan_features.
    
    Some drivers set vlan acceleration features in vlan_features.  This causes
    issues with Q-in-Q/802.1ad configurations.
    
    Audit all the drivers for correct vlan_features.  Fix broken ones.
    Add a warning to vlan code to help catch future offenders.
    ====================
    
    Signed-off-by: David S. Miller <davem@davemloft.net>

--- include/linux/netdev_features.h
+++ include/linux/netdev_features.h
@@ -91,4 +91,11 @@ typedef u32 netdev_features_t;
 /* changeable features with no special hardware requirements */
 #define NETIF_F_SOFT_FEATURES	(NETIF_F_GSO | NETIF_F_GRO)
 
+#define NETIF_F_VLAN_FEATURES	(NETIF_F_HW_VLAN_CTAG_FILTER | \
+				 NETIF_F_HW_VLAN_CTAG_RX | \
+				 NETIF_F_HW_VLAN_CTAG_TX | \
+				 NETIF_F_HW_VLAN_STAG_FILTER | \
+				 NETIF_F_HW_VLAN_STAG_RX | \
+				 NETIF_F_HW_VLAN_STAG_TX)
+
 #endif	/* _LINUX_NETDEV_FEATURES_H */
