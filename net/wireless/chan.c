benji-cannot-code/fast-vc-linux-benchmark-run-3

cfg80211: track monitor channel
Make it even more obvious we support single
monitor channel. This will allow us to remove
.get_channel.

Signed-off-by: Michal Kazior <michal.kazior@tieto.com>
Signed-off-by: Johannes Berg <johannes.berg@intel.com>

net/wireless/chan.c
@@ -83,6 +83,7 @@ int cfg80211_set_monitor_channel(struct cfg80211_registered_device *rdev,
 				 int freq, enum nl80211_channel_type chantype)
 {
 	struct ieee80211_channel *chan;
+	int err;
 
 	if (!rdev->ops->set_monitor_channel)
 		return -EOPNOTSUPP;
@@ -93,7 +94,13 @@ int cfg80211_set_monitor_channel(struct cfg80211_registered_device *rdev,
 	if (!chan)
 		return -EINVAL;
 
-	return rdev->ops->set_monitor_channel(&rdev->wiphy, chan, chantype);
+	err = rdev->ops->set_monitor_channel(&rdev->wiphy, chan, chantype);
+	if (!err) {
+		rdev->monitor_channel = chan;
+		rdev->monitor_channel_type = chantype;
+	}
+
+	return err;
 }
 
 void

intika/Dorimanx-SG2-I9100-Kernel

net/wireless/chan.c
@@ -82,7 +82,7 @@ int cfg80211_set_freq(struct cfg80211_registered_device *rdev,
 		      enum nl80211_channel_type channel_type)
 {
 	struct ieee80211_channel *chan;
-	int result;
+	int err;
 
 	if (wdev && wdev->iftype == NL80211_IFTYPE_MONITOR)
 		wdev = NULL;
@@ -115,16 +115,17 @@ int cfg80211_set_freq(struct cfg80211_registered_device *rdev,
 		return -EINVAL;
 	}
 
-	result = rdev->ops->set_channel(&rdev->wiphy,
-					wdev ? wdev->netdev : NULL,
-					chan, channel_type);
-	if (result)
-		return result;
+	err = rdev->ops->set_channel(&rdev->wiphy,
+				     wdev ? wdev->netdev : NULL,
+				     chan, channel_type);
+	if (!err) {
+		if (wdev)
+			wdev->channel = chan;
+		rdev->monitor_channel = chan;
+		rdev->monitor_channel_type = channel_type;
+	}
 
-	if (wdev)
-		wdev->channel = chan;
-
-	return 0;
+	return err;
 }
 
 void