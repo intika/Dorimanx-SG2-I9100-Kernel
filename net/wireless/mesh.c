9b3af286a589a9f8458c73a3f22ce05ba3fcb41d
StanTRC <stanislav.kravchuk@gmail.com>
2014-04-15 12:29:15 +0400

    Revert "cfg80211: respect iface combinations when starting operation"
    This reverts commit 69d55e911cab4b9f547bb7c6ed41d36757c9490f.

--- net/wireless/mesh.c
+++ net/wireless/mesh.c
@@ -153,11 +153,6 @@ int __cfg80211_join_mesh(struct cfg80211_registered_device *rdev,
 					  setup->channel_type))
 		return -EINVAL;
 
-	err = cfg80211_can_use_chan(rdev, wdev, setup->channel,
-				    CHAN_MODE_SHARED);
-	if (err)
-		return err;
-
 	err = rdev->ops->join_mesh(&rdev->wiphy, dev, conf, setup);
 	if (!err) {
 		memcpy(wdev->ssid, setup->mesh_id, setup->mesh_id_len);
@@ -176,11 +171,9 @@ int cfg80211_join_mesh(struct cfg80211_registered_device *rdev,
 	struct wireless_dev *wdev = dev->ieee80211_ptr;
 	int err;
 
-	mutex_lock(&rdev->devlist_mtx);
 	wdev_lock(wdev);
 	err = __cfg80211_join_mesh(rdev, dev, setup, conf);
 	wdev_unlock(wdev);
-	mutex_unlock(&rdev->devlist_mtx);
 
 	return err;
 }
@@ -207,11 +200,6 @@ int cfg80211_set_mesh_freq(struct cfg80211_registered_device *rdev,
 		wdev_lock(wdev);
 		err = cfg80211_set_freq(rdev, wdev, freq, channel_type);
 		wdev_unlock(wdev);
-
-		err = cfg80211_can_use_chan(rdev, wdev, channel,
-					    CHAN_MODE_SHARED);
-		if (err)
-			return err;
 
 		if (!err)
 			wdev->channel = channel;
