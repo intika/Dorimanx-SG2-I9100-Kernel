9b238ab78f114e6b82c7b2eb7b396f79afee812a
gsstudios <josh.lay@exemail.com.au>
2015-10-26 15:06:34 +1100

    Fix derp in Samsung WL patch

--- include/linux/mmc/card.h
+++ include/linux/mmc/card.h
@@ -248,7 +248,7 @@ struct mmc_card {
 #define MMC_QUIRK_DISABLE_CD	(1<<5)		/* disconnect CD/DAT[3] resistor */
 #define MMC_QUIRK_INAND_CMD38	(1<<6)		/* iNAND devices have broken CMD38 */
 #define MMC_QUIRK_BLK_NO_CMD23	(1<<7)		/* Avoid CMD23 for regular multiblock */
-#define MMC_QUIRK_SAMSUNG_WL_PATCH     (1<<9)	/* Patch Samsung FW to fix wear leveling bug */
+#define MMC_QUIRK_SAMSUNG_WL_PATCH     (1<<8)	/* Patch Samsung FW to fix wear leveling bug */
 #define MMC_QUIRK_BROKEN_BYTE_MODE_512 (1<<9)	/* Avoid sending 512 bytes in */
 /* MoviNAND secure issue */
 #define MMC_QUIRK_LONG_READ_TIME (1<<10)		/* Data read time > CSD says */
