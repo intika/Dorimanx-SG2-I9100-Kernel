ba00776b7ea932a8a9cb5dd9634a9e27ccf05d49
gsstudios <josh.lay@exemail.com.au>
2016-07-04 11:56:37 +1000

    Revert "Fix CONFIG_HZ dependency in wifi driver."
    This reverts commit 6d09d829ba969edbe4ed294d583b0dab73aabbae.

--- drivers/net/wireless/bcmdhd/wl_cfg80211.c
+++ drivers/net/wireless/bcmdhd/wl_cfg80211.c
@@ -8549,7 +8549,7 @@ wl_cfg80211_netdev_notifier_call(struct notifier_block * nb,
 				if (refcnt%5 == 0)
 					WL_ERR(("[NETDEV_DOWN] work_pending (%d th)\n", refcnt));
 				set_current_state(TASK_INTERRUPTIBLE);
-				schedule_timeout(HZ);
+				schedule_timeout(100);
 				set_current_state(TASK_RUNNING);
 				refcnt++;
 			}
