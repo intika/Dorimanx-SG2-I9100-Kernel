4edd726842fd15d71684457fb2d85e96ffef184c
alex <alextsanisbadlorg@gmail.com>
2016-07-15 08:12:35 +0000

    Revert "Revert "IO scheduler, lionheart restored""
    This reverts commit caaff10a6c2c1953b61b3ac5b3dfa4330dac13cf.
    
    Conflicts:
    	arch/arm/configs/dorimanx_defconfig

--- include/linux/cpufreq.h
+++ include/linux/cpufreq.h
@@ -437,6 +437,9 @@ extern struct cpufreq_governor cpufreq_gov_darkness;
 #elif defined(CONFIG_CPU_FREQ_DEFAULT_GOV_NEOX)
 extern struct cpufreq_governor cpufreq_gov_neox;
 #define CPUFREQ_DEFAULT_GOVERNOR  (&cpufreq_gov_neox)
+#elif defined(CONFIG_CPU_FREQ_DEFAULT_GOV_LIONHEART)
+extern struct cpufreq_governor cpufreq_gov_lionheart;
+#define CPUFREQ_DEFAULT_GOVERNOR  (&cpufreq_gov_lionheart)
 #endif
 
 /*********************************************************************
