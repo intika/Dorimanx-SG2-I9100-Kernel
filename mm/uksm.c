6158bd9e572a9704819d81d2ce72b1b2391901c0
Realex-fire <goy2010@yandex.ru>
2016-07-21 02:04:30 +0700

    Fixed compilation uksm

--- mm/uksm.c
+++ mm/uksm.c
@@ -180,7 +180,6 @@ static int is_full_zero(const void *s1, size_t len)
 }
 #endif
 
-#define U64_MAX		(~((u64)0))
 #define UKSM_RUNG_ROUND_FINISHED  (1 << 0)
 #define TIME_RATIO_SCALE	10000
 
@@ -1604,7 +1603,7 @@ static inline int check_collision(struct rmap_item *rmap_item,
 static struct page *page_trans_compound_anon(struct page *page)
 {
 	if (PageTransCompound(page)) {
-		struct page *head = compound_trans_head(page);
+		struct page *head = compound_head(page);
 		/*
 		 * head may actually be splitted and freed from under
 		 * us but it's ok here.
